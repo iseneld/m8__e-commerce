3ac0e50c65a9a007496d6fad202e0fdad3d02db4
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-08 10:48:53 +0200

    Trying to fix Router.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { Router, Route } from "react-router-dom";
-import Main from "./Pages/Main";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 import "./App.css";
-
-// It seems Router is the new "Routes" tag in React Router DOM syntax.
-// "Routes" isn't available, and "Router" seems to be working.
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route path="/" element={<Main />} />
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,14 @@
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 
-const Main = () => {
+const Home = () => {
   return (
     <>
     <Header />
-    <h1>Home</h1>
+      <h1>Home</h1>
     <Footer />
     </>
   )
 }
 
-export default Main;
+export default Home;
