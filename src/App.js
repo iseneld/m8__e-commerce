a1e110cab3a6400f6e83b1da6d98ec28ec3b0e61
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-08 10:10:36 +0200

    Footer and header added + folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <footer>
+      <h1>
+        Footer Test
+      </h1>
+    </footer>
+  )
+}
+
+export default Header;
