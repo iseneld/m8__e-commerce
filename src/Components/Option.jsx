631533f5eb89f1cba591eb74efdfbf293c7bc5ee
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-12 11:42:40 +0200

    Payment components added.

---
+++ src/Components/Option.jsx
@@ -0,0 +1,9 @@
+const Option = () => (
+  <article className="option">
+    <img src="" alt="Coins" />
+    <h2>100 XC</h2>
+    <button>$5</button>
+  </article>
+);
+
+export default Option;
--- src/Pages/Payment.jsx
+++ src/Pages/Payment.jsx
@@ -1,10 +1,19 @@
+import Option from '../Components/Option';
 import "../Styles/payment.css";
 
 const Payment = () => {
   return (
     <>
     <section>
-      <h1 className="hej">Payment</h1>
+      <h1 className="hej">Buy XTREME Coins</h1>
+        <section className="shop">
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+          <Option />
+      </section>
     </section>
       
     </>
