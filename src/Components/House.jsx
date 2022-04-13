f32b7adfd3c6f39388fc9660f0b52d92806a681d
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-13 12:00:09 +0200

    Front page polished and such.

--- src/Components/House.jsx
+++ src/Components/House.jsx
@@ -1,22 +1,17 @@
 import Floor from './Floor'
 
 const House = () => (
-        <section className="house">
-        <Floor />
-        <Floor />
-        <Floor />
-        <Floor />
-        <Floor />
-        <Floor />
-        <Floor />
-        <Floor />
-        <Floor />
-        <Floor />
-        <Floor />
-        <section className="entrance">
-          <article className="door"></article>
-        </section>
-      </section>
+  <section className="house">
+    <Floor />
+    <Floor />
+    <Floor />
+    <Floor />
+    <Floor />
+    <Floor />
+    <section className="entrance">
+      <article className="door"></article>
+    </section>
+  </section>
 );
 
 export default House;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,13 @@
 import House from '../Components/House';
 
-const Home = () => {
-  return (
-    <>
-      <div className="background">
-        <main>
-          <House />
-        </main>
-      </div>
-    </>
-  )
-}
+const Home = () => (
+  <>
+    <div className="background">
+      <main>
+        <House />
+      </main>
+    </div>
+  </>
+);
 
 export default Home;
