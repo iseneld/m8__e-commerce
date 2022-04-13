36289276339c7e13b0182b509bbd3fbc06f413cb
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-13 14:54:05 +0200

    Pay1 and Breadcrumb styling added.

--- src/Components/Breadcrumb.jsx
+++ src/Components/Breadcrumb.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Breadcrumb = () => {
   return (
-    <nav>
+    <nav className="breadcrumb">
       <Link to="/pay1">Select Payment</Link> {" > "} 
       <Link to="/pay2">Payment Information</Link> {" > "} 
       <Link to="/pay3">Order Confirmation</Link>
