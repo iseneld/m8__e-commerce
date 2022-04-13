09ca6ed143b100a61a5812ca79fb1e301e7f0fda
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-13 14:44:32 +0200

    trying to add popup

--- src/Components/Window.jsx
+++ src/Components/Window.jsx
@@ -1,5 +1,14 @@
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
 const Window = () => (
-  <article className="window"></article>
+  <article className="window">
+    {/*     <Popup trigger={<button> Click to open popup </button>}
+      position="right center">
+      <div>GeeksforGeeks</div>
+      <button>Click here</button>
+    </Popup> */}
+  </article>
 );
 
 export default Window;
