2fc345ad1c6f35af7d3b6bbb2df160292baf06b5
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-13 14:10:18 +0200

    Scss cleaned up. House styling moved to spearate doc.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,16 +5,16 @@ const Header = () => {
   return (
     <header>
       <h1>
-        <Link to="/"><img src="./images/logo.png" alt="The Hills logo" /></Link>
+        <Link to="/"><img src="./images/logo.png" alt="The Hills logo" className="logo"/></Link>
       </h1>
       <div>
         <section>
-          <img src="./images/coin.png" alt="Currency icon" class="icon"/>
+          <img src="./images/coin.png" alt="Currency icon" className="icon"/>
           <p>500</p>
         </section>
         <nav>
-          <Link to="/pay1"><img src="./images/pay.png" alt="Payment icon" class="icon" /></Link>
-          <img src="./images/user.png" alt="User icon" class="icon"/>
+          <Link to="/pay1"><img src="./images/pay.png" alt="Payment icon" className="icon" /></Link>
+          <img src="./images/user.png" alt="User icon" className="icon"/>
         </nav>
       </div>
       
