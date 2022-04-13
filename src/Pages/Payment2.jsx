5ec135d6e28e27642a3538b822376f3a43435c0a
iseneld <84731866+iseneld@users.noreply.github.com>
2022-04-13 11:01:55 +0200

    HTML for Payment step 2 in place.

--- src/Pages/Payment2.jsx
+++ src/Pages/Payment2.jsx
@@ -1,4 +1,5 @@
 import Option from '../Components/Option';
+import { Link } from 'react-router-dom';
 import "../Styles/payment.css";
 
 const Payment2 = () => {
@@ -8,7 +9,24 @@ const Payment2 = () => {
       <h6>Select Payment{">"} <strong>Payment Information</strong> {">"} Order Confirmation</h6>
       <h1 className="hej">Payment Information</h1>
         <section className="shop2">
-          <Option />
+          <section className="shop2-left">
+            <Option />
+            <table>
+              <tr>
+                <td>Order</td>
+                <td>1000XTC</td>
+              </tr>
+              <tr>
+                <td>Total</td>
+                <td>$35</td>
+              </tr>
+            </table>
+            <footer>
+              All prices are inclusive of VAT if applicable. <br />
+              XTREME Coins limited, Stockholm
+            </footer>
+          </section>
+          
            <form>
               <label for="cardtype">Card Type</label>
               <select id="cardtype" name="cardtype"></select>
@@ -22,8 +40,10 @@ const Payment2 = () => {
               </div>
               <label for="cardholder">Card Holder</label>
               <input type="text" id="cardholder" name="cardholder" />
-              back
-              pay
+              <input type="checkbox" id="remember" name="remember" />
+              <label for="remember">Save payment method for later use</label>
+              <Link to="/">Back</Link>
+              <input type="Submit" id="paybtn" name="paybtn" value="Pay"/>
           </form>
       </section>
     </section>
