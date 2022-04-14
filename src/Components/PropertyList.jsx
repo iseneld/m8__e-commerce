4935821dcc574eef17be72e31d799d60829ca51a
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-14 16:08:27 +0200

    small changes to propertylist

--- src/Components/PropertyList.jsx
+++ src/Components/PropertyList.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const PropertyList = () => {
     return (
         <>
-            <ul>
+            <ul className="user-properties-listed">
                 <li className="property-list">
                     <img src={require("../Styles/css-content/lgh2.jpeg")} />
                     <section className="about-property">
@@ -14,6 +14,21 @@ const PropertyList = () => {
                             <li>3 rooms</li>
                             <li>89 m2</li>
                         </ul>
+                    </section>
+                </li>
+            </ul>
+
+            <ul className="user-properties-listed">
+                <li className="property-list">
+                    <img src={require("../Styles/css-content/lgh2.jpeg")} />
+                    <section className="about-property">
+                        <h2>Apartment 109</h2>
+                        <h5>Floor: </h5>
+                        <section className="about-property-list">
+                            <p>789 XC</p>
+                            <p>3 rooms</p>
+                            <p>89 Xm2</p>
+                        </section>
                     </section>
                 </li>
             </ul>
--- src/Components/UserProperties.jsx
+++ src/Components/UserProperties.jsx
@@ -4,19 +4,9 @@ import PropertyList from './PropertyList';
 const UserProperties = () => {
     return (
         <>
-            <div className="apt-display">
-                <section className="user-properties">
-                    <section className="title-icon">
-                        <h1>User Properties</h1>
-                        <div className="store-icon"></div>
-                    </section>
-                    <section className="property-display">
-                        <PropertyList />
-                    </section>
-                </section>
-
-            </div>
-
+            <section className="user-properties">
+                <PropertyList />
+            </section>
         </>
     )
 }
