2a5890f348ce484e3a3dd61ff3c3f0b2c6529720
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-14 11:42:47 +0200

    fixed apartment view css

--- src/Components/AptDisplay.jsx
+++ src/Components/AptDisplay.jsx
@@ -5,7 +5,7 @@ const AptDisplay = () => {
             <div className="apt-display">
                 <h1 className="apt-title">Apartment 109</h1>
                 <h3 className="sale-status">This property is for sale!</h3>
-                <img src='./images/apt.png' alt="The apartment"/>
+                <img src='./images/apt.png' alt="The apartment" />
                 <section className="about-apt">
                     <h4>ID:</h4>
                     <h4>Owner:</h4>
