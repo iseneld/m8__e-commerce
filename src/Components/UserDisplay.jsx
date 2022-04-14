16d90f6e3c0695d08cf49cbe7e44abce6ed4dd47
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-14 15:27:51 +0200

    about user fixed with styling

---
+++ src/Components/UserDisplay.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import UserProperties from './UserProperties';
+
+const UserDisplay = () => {
+    return (
+        <>
+            <section className="profile-pages">
+                <section className="about-user">
+                    <div className="profile-pic">
+                    </div>
+                    <h1 className="user-title">User name</h1>
+                    <div className="line"></div>
+                    <ul className="user-list">
+                        <li>User since:</li>
+                        <li>Properties owned:</li>
+                        <li>Properties sold:</li>
+                    </ul>
+                </section>
+                <section className="user-content">
+                    <UserProperties />
+                </section>
+            </section>
+
+
+        </>
+    )
+}
+
+export default UserDisplay;
