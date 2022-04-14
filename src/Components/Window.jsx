d7c9ee1f898313b089f921268f6db305506df3dc
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-14 11:30:35 +0200

    pop-up working

--- src/Components/Window.jsx
+++ src/Components/Window.jsx
@@ -1,13 +1,29 @@
 import AptPopUp from './AptPopUp';
 import Popup from 'reactjs-popup';
+import { Link } from 'react-router-dom';
 
 const Window = () => (
   <article className="window" /* onClick={AptPopUp} */>
-    <Popup trigger={<button>Click here</button>}
+    <Popup trigger={<button className="window-button"></button>}
       position="right center" open={true}>
+
       <div className="pop-up">
-        <h1>hej</h1>
-        <p>blabla</p>
+        <h1>Apartment 109</h1>
+        <h3>This property is for sale!</h3>
+        <section className="about-property">
+          <ul>
+            <li>ID:</li>
+            <li>Prize:</li>
+            <li>Your coins:</li>
+          </ul>
+        </section>
+
+        <section className="buttons">
+          <Link to="/apartment">
+            <button>VIEW</button>
+          </Link>
+          <button>BUY</button>
+        </section>
       </div>
     </Popup>
   </article>
--- src/Pages/Apartment.jsx
+++ src/Pages/Apartment.jsx
@@ -1,5 +1,6 @@
 import AptDisplay from "../Components/AptDisplay";
 import "../Styles/css-content/lgh1.jpeg";
+import "../Styles/apartment.css";
 
 
 const Apartment = () => {
