7fd6370ecac962f84e85d3375a694803c2c95927
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-14 16:32:46 +0200

    added link to icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Link  } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
       <h1>
-        <Link to="/"><img src="./images/logo.png" alt="The Hills logo" className="logo"/></Link>
+        <Link to="/"><img src="./images/logo.png" alt="The Hills logo" className="logo" /></Link>
       </h1>
       <div>
         <section>
-          <img src="./images/coin.png" alt="Currency icon" className="icon"/>
+          <img src="./images/coin.png" alt="Currency icon" className="icon" />
           <p>500</p>
         </section>
         <nav>
           <Link to="/pay1"><img src="./images/pay.png" alt="Payment icon" className="icon" /></Link>
-          <img src="./images/user.png" alt="User icon" className="icon"/>
+          <Link to="/user"><img src="./images/user.png" alt="User icon" className="icon" /></Link>
         </nav>
       </div>
-      
+
     </header>
   )
 }
