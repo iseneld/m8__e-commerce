661be6048bbcc3221b69e7480f802b8c27da4dc3
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-21 10:37:38 +0200

    took away unneccessary div

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -18,56 +18,46 @@ function RegisterForm() {
         <form className="form"> {/* onSubmit={onSubmit} */}
 
           <h5 className="form-title">UserName</h5>
-          <div className="form-group">
-            <input
-              type='text'
-              className="form-control"
-              id="username"
-              name="username"
-              value={username}
-              placeholder="Username"
-            //   onChange={onChange}
-            />
-          </div>
+          <input
+            type='text'
+            className="form-control"
+            id="username"
+            name="username"
+            value={username}
+            placeholder="Username"
+          //   onChange={onChange}
+          />
           <h5 className="form-title">Email</h5>
-          <div className="form-group">
-            <input
-              type='email'
-              className="form-control"
-              id="email"
-              name="email"
-              value={email}
-              placeholder="Email"
-            //   onChange={onChange}
-            />
-          </div>
+          <input
+            type='email'
+            className="form-control"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+          //   onChange={onChange}
+          />
           <h5 className="form-title">Password</h5>
-          <div className="form-group">
-            <input
-              type='password'
-              className="form-control"
-              id="password"
-              name="password"
-              value={password}
-              placeholder="Password"
-            //   onChange={onChange}
-            />
-          </div>
+          <input
+            type='password'
+            className="form-control"
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+          //   onChange={onChange}
+          />
           <h5 className="form-title">Confirm password</h5>
-          <div className="form-group">
-            <input
-              type='password'
-              className="form-control"
-              id="password2"
-              name="password2"
-              value={password2}
-              placeholder="Confirm password"
-            //   onChange={onChange}
-            />
-          </div>
-          <div className="form-group">
-            <button type="submit" className="btn">Continue</button>
-          </div>
+          <input
+            type='password'
+            className="form-control"
+            id="password2"
+            name="password2"
+            value={password2}
+            placeholder="Confirm password"
+          //   onChange={onChange}
+          />
+          <button type="submit" className="btn">Continue</button>
         </form>
       </section>
       {/*       </section> */}
