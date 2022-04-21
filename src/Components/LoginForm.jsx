218c73cbdd43eb1806ee61081462b700f3d4ad9a
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-21 13:51:27 +0200

    loginpage fixed

---
+++ src/Components/LoginForm.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+import { BiArrowBack } from "react-icons/bi";
+
+function LoginForm() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    const { email, password } = formData
+
+    return (
+        <section className="wrapper-content">
+            {/*       <section className="form-wrapper"> */}
+            <section className="register-form register-form--login-form">
+                <h1 className="heading heading--formHeading heading--loginHeading">Login</h1>
+                <form className="form"> {/* onSubmit={onSubmit} */}
+                    <h5 className="form-title">Email</h5>
+                    <input
+                        type='email'
+                        className="form-control"
+                        id="email"
+                        name="email"
+                        value={email}
+                        placeholder="Email"
+                    //   onChange={onChange}
+                    />
+                    <h5 className="form-title">Password</h5>
+                    <input
+                        type='password'
+                        className="form-control"
+                        id="password"
+                        name="password"
+                        value={password}
+                        placeholder="Password"
+                    //   onChange={onChange}
+                    />
+                    <p className="forgot-txt">Forgot Password?</p>
+                    <div className="btns">
+                        <button className="back-btn"><BiArrowBack size={30} /></button>
+                        <button type="submit" className="btn">Continue</button>
+                    </div>
+                </form>
+            </section>
+            {/*       </section> */}
+        </section>
+    )
+};
+
+export default LoginForm;
---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "../Components/LoginForm";
+import '../Styles/main.css';
+
+function LoginPage() {
+    return (
+        <>
+            <section className="wrapper">
+                <LoginForm />
+            </section>
+        </>
+    )
+};
+
+export default LoginPage;
