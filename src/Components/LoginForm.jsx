3730e7561398c3f1396d027000e29e1eb687a9ed
johk <johkli19@hotmail.se>
2022-04-22 14:14:11 +0200

    working login, storing in localStorage

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -3,27 +3,57 @@ import { BiArrowBack } from "react-icons/bi";
 
 function LoginForm() {
     const [formData, setFormData] = useState({
-        email: '',
+        username: '',
         password: '',
-    })
+    });
 
-    const { email, password } = formData
+    const { username, password } = formData;
+
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        // Put settings into another helper JS file, register too
+        const settings = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        }
+
+        try {
+            const loginUser = await fetch('http://localhost:5000/users/login', settings);
+            const user = await loginUser.json();
+            localStorage.setItem('currentUser', JSON.stringify({token: user.token, userName: user.username}));
+
+        } catch(err) {
+            alert("Incorrect input data. Please try something else.");
+            console.log(err);
+        }
+
+
+        // console.log(JSON.parse(localStorage.getItem('currentUser')));
+    }
 
     return (
         <section className="wrapper-content">
             {/*       <section className="form-wrapper"> */}
             <section className="register-form register-form--login-form">
                 <h1 className="heading heading--formHeading heading--loginHeading">Login</h1>
-                <form className="form"> {/* onSubmit={onSubmit} */}
-                    <h5 className="form-title">Email</h5>
+                <form className="form" onSubmit={onSubmit} > 
+                    <h5 className="form-title">Username</h5>
                     <input
-                        type='email'
+                        type='text'
                         className="form-control"
-                        id="email"
-                        name="email"
-                        value={email}
-                        placeholder="Email"
-                    //   onChange={onChange}
+                        id="username"
+                        name="username"
+                        value={username}
+                        placeholder="Username"
+                       onChange={onChange}
+                       required
                     />
                     <h5 className="form-title">Password</h5>
                     <input
@@ -33,7 +63,8 @@ function LoginForm() {
                         name="password"
                         value={password}
                         placeholder="Password"
-                    //   onChange={onChange}
+                       onChange={onChange}
+                       required
                     />
                     <p className="forgot-txt">Forgot Password?</p>
                     <div className="btns">
