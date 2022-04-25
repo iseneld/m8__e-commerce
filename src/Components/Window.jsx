f25a0544893a6a16664e6d2a873895d35f12685b
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-25 14:17:36 +0200

    pop-up basic styling added

--- src/Components/Window.jsx
+++ src/Components/Window.jsx
@@ -2,25 +2,30 @@ import AptPopUp from './AptPopUp';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import { Link } from 'react-router-dom';
+import apartmentIMG from "../Styles/css-content/lgh1.jpeg";
 
 const Window = () => (
   <>
     <section className="window">
       <Popup trigger={<button className="window-btn"> </button>}
-        position="right center">
+        position="right center" contentStyle={{ width: "300px" }}>
         <div>
           <h1>Apartment 1</h1>
-          <h3>This property is for sale!</h3>
-          <section className="abt-apartment">
-            <ul>
-              <li>ID: </li>
-              <li>Prize:</li>
-              <li>Your coins:</li>
-            </ul>
-          </section>
-          <section className="apt-buttons">
-            <Link to="/apartment"><button>VIEW</button></Link>
-            <button>BUY</button>
+          <section className="pop-up-apt-display">
+            <div className="pop-up-apt-image">
+              <img src={apartmentIMG}></img>
+            </div>
+            <section className="abt-apartment">
+              <h3>For sale!</h3>
+              <ul>
+                <li><h4>Prize:</h4></li>
+                <li><h4>Your coins:</h4></li>
+              </ul>
+            </section>
+            <section className="apt-buttons">
+              <Link to="/apartment"><button>VIEW</button></Link>
+              <button>BUY</button>
+            </section>
           </section>
         </div>
 
