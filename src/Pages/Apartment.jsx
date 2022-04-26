129a60d746b77665a03afce441ab61686aa503c7
sammydjohari <djoharisammy@gmail.com>
2022-04-26 14:37:58 +0200

    fixed bug

--- src/Pages/Apartment.jsx
+++ src/Pages/Apartment.jsx
@@ -1,13 +1,11 @@
 import AptDisplay from "../Components/AptDisplay";
 import "../Styles/css-content/lgh1.jpeg";
-import Inventory from "../Components/Inventory";
 
 
 const Apartment = () => {
     return (
         <>
             <div className="article-container">
-                <Inventory />
                 <AptDisplay />
             </div>
 
