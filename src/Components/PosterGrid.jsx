b0a9b2f46ce5e48681a35457f5a030d4473adf17
johk <johkli19@hotmail.se>
2022-04-26 15:24:32 +0200

    poster components added

---
+++ src/Components/PosterGrid.jsx
@@ -0,0 +1,13 @@
+import Poster from "./Poster";
+
+function PosterGrid() {
+  return (
+    <div className="poster-grid">
+          <Poster />
+          <Poster />
+          <Poster />
+    </div>
+  )
+}
+
+export default PosterGrid;
