27bf4f7d5a12a4e1f09edb79ab6e375c60a8f3fd
sammydjohari <djoharisammy@gmail.com>
2022-04-26 14:36:53 +0200

    push inc

--- src/Pages/Apartment.jsx
+++ src/Pages/Apartment.jsx
@@ -1,12 +1,13 @@
 import AptDisplay from "../Components/AptDisplay";
 import "../Styles/css-content/lgh1.jpeg";
-
+import Inventory from "../Components/Inventory";
 
 
 const Apartment = () => {
     return (
         <>
             <div className="article-container">
+                <Inventory />
                 <AptDisplay />
             </div>
 
