25f35f96ba8bc50d3a0d8850afb7da48d1f10f18
johk <johkli19@hotmail.se>
2022-04-28 23:35:11 +0200

    moving cloud parallax

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react';
 import House from '../Components/House';
 
 const Home = ({apartments}) => {
   const apartmentsProp = apartments;
 
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [])
+
+
   return (
   <>
     {/* <div className="background"> */}
       <main>
-        <House apartments={apartmentsProp}/>
+        <div className='wrapper'>
+          <img src='./images/clouds.png' className='clouds' 
+          style={{ transform: `translateX(${offsetY * 0.5}px)` }} />
+          <House apartments={apartmentsProp}/>
+        </div>
       </main>
     {/* </div> */}
   </>);
