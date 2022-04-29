8dfefad5983524896a91e59b1013a5c90b7ead26
SofiaEmt <sofia.emteryd@gmail.com>
2022-04-29 13:23:58 +0200

    now the poster can be put but it does not stick

--- src/Components/Poster.jsx
+++ src/Components/Poster.jsx
@@ -1,6 +1,24 @@
-function Poster() {
+
+
+function Poster({ changeContainer, sentPoster, posterId }) {
+
+  const poster = sentPoster;
+  let localPoster = localStorage.getItem("poster" + posterId);
+  console.log(localPoster)
+
+  if (localPoster === null) localPoster = "";
+
+  if (poster !== "") {
+    localStorage.setItem("poster" + posterId, poster)
+  }
+
+  //
+  //poster === "" ? null : localStorage;
+
   return (
-    <div className="poster-frame">Poster</div>
+    <div className="poster-frame" onClick={changeContainer}>
+      <img src={poster === "" ? require(`../Styles/css-content/posters/${localStorage.getItem("poster" + posterId)}`) :
+        require(`../Styles/css-content/posters/${poster}`)} /></div>
   )
 }
 
--- src/Components/PosterGrid.jsx
+++ src/Components/PosterGrid.jsx
@@ -1,11 +1,20 @@
 import Poster from "./Poster";
+import { useState, useContext } from 'react';
+import { PosterContext } from "../Pages/Apartment";
 
 function PosterGrid() {
+
+  const [selectedPosterContainer, setSelectedPosterContainer] = useState();
+  const { selectedPoster } = useContext(PosterContext);
+
+
+
+
   return (
     <div className="poster-grid">
-          <Poster />
-          <Poster />
-          <Poster />
+      <Poster changeContainer={() => setSelectedPosterContainer(1)} sentPoster={selectedPosterContainer === 1 ? selectedPoster : ""} posterId={1} />
+      <Poster changeContainer={() => setSelectedPosterContainer(2)} sentPoster={selectedPosterContainer === 2 ? selectedPoster : ""} posterId={2} />
+      <Poster changeContainer={() => setSelectedPosterContainer(3)} sentPoster={selectedPosterContainer === 3 ? selectedPoster : ""} posterId={3} />
     </div>
   )
 }
