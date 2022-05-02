3f7e23d7dcf88e029fb85b7ff99c3f61e4626a94
SofiaEmt <sofia.emteryd@gmail.com>
2022-05-02 15:12:13 +0200

    adde content and styling to the content
    added a new component as well

--- src/Components/AboutDisplay.jsx
+++ src/Components/AboutDisplay.jsx
@@ -1,15 +1,12 @@
-import { Link } from 'react-router-dom';
-
+import AboutContent from './AboutContent';
 
 const AboutDisplay = () => {
     return (
         <div className="profile-container">
-        <div className='user-container'>
-            <div className="apt-display">
-                    <section className="about-content">
-                            <h1 className="user-props-title">About Us</h1>
-                    </section>
-              </div>
+            <div className='user-container'>
+                <div className="apt-display">
+                    <AboutContent />
+                </div>
             </div>
         </div>
     )
