34ae9b71e303328bc6040901fd5f6df705236db8
sammydjohari <djoharisammy@gmail.com>
2022-05-03 16:10:38 +0200

    animations fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -35,19 +35,19 @@ const Home = ({ apartments }) => {
       <main>
         <div className='parallax-wrapper'>
           <img src='./images/ufo.png' className='ufo'
-            style={{ transform: `translateX(${offsetY * -3.2}px)` }} alt="UFO flying in the air" />
+            style={{ transform: `translateX(${offsetY * 3.2}px)` }} alt="UFO flying in the air" />
             <img src='./images/rocket.png' className='rocket'
-            style={{ transform: `translateX(${offsetY * 3.2}px) translateY(${offsetY * -3.2}px)` }} alt="Rocket flying in the air" />
+            style={{ transform: `translateX(${offsetY * -4.2}px) translateY(${offsetY * 1.2}px)` }} alt="Rocket flying in the air" />
             <img src='./images/metheo.png' className='metheo'
-            style={{ transform: `translateX(${offsetY * 3.2}px) translateY(${offsetY * 1.2}px)` }} alt="Metheorite flying in the air" />
+            style={{ transform: `translateX(${offsetY * -2.2}px) translateY(${offsetY * -1.2}px)` }} alt="Metheorite flying in the air" />
           <img src='./images/ironman.png' className='ironman'
-            style={{ transform: `translateX(${offsetY * 2}px)` }} alt="Iron Man flying in the air" />
+            style={{ transform: `translateX(${offsetY * -2}px)` }} alt="Iron Man flying in the air" />
           <img src='./images/clouds.png' className='clouds2'
-            style={{ transform: `translateX(${offsetY * -0.05}px)` }} alt="Cloud flying in the air" />
-          <img src='./images/clouds.png' className='clouds3'
             style={{ transform: `translateX(${offsetY * 0.05}px)` }} alt="Cloud flying in the air" />
+          <img src='./images/clouds.png' className='clouds3'
+            style={{ transform: `translateX(${offsetY * -0.05}px)` }} alt="Cloud flying in the air" />
           <img src='./images/clouds.png' className='clouds4'
-            style={{ transform: `translateX(${offsetY * 0.07}px)` }} alt="Cloud flying in the air" />
+            style={{ transform: `translateX(${offsetY * -0.07}px)` }} alt="Cloud flying in the air" />
         </div>
         <House apartments={apartmentsProp} />
         <div ref={pageEndRef} />
