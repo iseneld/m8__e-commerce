ab9a8e59a43a1f899f8f48d79234187a15f24ca4
SofiaEmt <sofia.emteryd@gmail.com>
2022-05-03 11:46:13 +0200

    Popup on main page added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import House from '../Components/House';
+import AboutPop from './AboutPop';
 
-const Home = ({apartments}) => {
+
+const Home = ({ apartments }) => {
   const apartmentsProp = apartments;
 
   const [offsetY, setOffsetY] = useState(0);
@@ -15,25 +17,27 @@ const Home = ({apartments}) => {
 
 
   return (
-  <>
-    {/* <div className="background"> */}
+    <>
+      {/* <div className="background"> */}
+
+      <AboutPop />
       <main>
         <div className='parallax-wrapper'>
-          <img src='./images/ufo.png' className='ufo' 
-          style={{ transform: `translateX(${offsetY * -3.2}px)` }} alt="UFO flying in the air"/>
-          <img src='./images/ironman.png' className='ironman' 
-          style={{ transform: `translateX(${offsetY * 2}px)` }} alt="Iron Man flying in the air"/>
-          <img src='./images/clouds.png' className='clouds2' 
-          style={{ transform: `translateX(${offsetY * -0.05}px)` }} alt="Cloud flying in the air" />
-          <img src='./images/clouds.png' className='clouds3' 
-          style={{ transform: `translateX(${offsetY * 0.05}px)` }} alt="Cloud flying in the air"  />
-          <img src='./images/clouds.png' className='clouds4' 
-          style={{ transform: `translateX(${offsetY * 0.07}px)` }} alt="Cloud flying in the air"  />
+          <img src='./images/ufo.png' className='ufo'
+            style={{ transform: `translateX(${offsetY * -3.2}px)` }} alt="UFO flying in the air" />
+          <img src='./images/ironman.png' className='ironman'
+            style={{ transform: `translateX(${offsetY * 2}px)` }} alt="Iron Man flying in the air" />
+          <img src='./images/clouds.png' className='clouds2'
+            style={{ transform: `translateX(${offsetY * -0.05}px)` }} alt="Cloud flying in the air" />
+          <img src='./images/clouds.png' className='clouds3'
+            style={{ transform: `translateX(${offsetY * 0.05}px)` }} alt="Cloud flying in the air" />
+          <img src='./images/clouds.png' className='clouds4'
+            style={{ transform: `translateX(${offsetY * 0.07}px)` }} alt="Cloud flying in the air" />
         </div>
-        <House apartments={apartmentsProp}/>
+        <House apartments={apartmentsProp} />
       </main>
-    {/* </div> */}
-  </>);
+      {/* </div> */}
+    </>);
 };
 
 export default Home;
