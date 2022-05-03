b524aa78ed811e84d21de8e60a406a41db2ccb73
Muridae-dev <nhlund@gmail.com>
2022-05-03 15:59:32 +0200

    changes

--- src/Pages/MarketplacePage.jsx
+++ src/Pages/MarketplacePage.jsx
@@ -4,11 +4,9 @@ import Marketplace from '../Components/Marketplace';
 function MarketplacePage() {
   return (
     <div className="profile-container">
-      <div className='user-container'>
         <div className="apt-display">
           <Marketplace />
         </div>
-      </div>
     </div>
   )
 }
