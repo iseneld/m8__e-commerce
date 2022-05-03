1cf4db72634a27c38416dec0afcc6b38628fe6f0
SofiaEmt <sofia.emteryd@gmail.com>
2022-05-03 13:38:05 +0200

    marketplace

--- src/Components/Marketplace.jsx
+++ src/Components/Marketplace.jsx
@@ -6,8 +6,37 @@ const Marketplace = () => {
             <h1 className="user-props-title">Marketplace</h1>
             <section className="marketplace">
                 <section className="furniture">
+                    <h2>Properties</h2>
+                    <section className="objects-section">
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                    </section>
+                </section>
+                <section className="furniture">
                     <h2>Furniture</h2>
                     <section className="objects-section">
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
                         <div className="marketplace-objects"></div>
                         <div className="marketplace-objects"></div>
                         <div className="marketplace-objects"></div>
@@ -21,6 +50,14 @@ const Marketplace = () => {
                 <section className="furniture">
                     <h2>Posters</h2>
                     <section className="objects-section">
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
+                        <div className="marketplace-objects"></div>
                         <div className="marketplace-objects"></div>
                         <div className="marketplace-objects"></div>
                         <div className="marketplace-objects"></div>
--- src/Pages/Payment1.jsx
+++ src/Pages/Payment1.jsx
@@ -1,12 +1,12 @@
 import Option from '../Components/Option';
-import Breadcrumb from '../Components/Breadcrumb'; 
+import Breadcrumb from '../Components/Breadcrumb';
 
 const Payment1 = () => {
   return (
     <>
-    <section className="shop-wrap">
-      <Breadcrumb />
-      <h1>Buy XTREME Coins</h1>
+      <section className="shop-wrap">
+        <Breadcrumb />
+        <h1>Buy Hill Coins</h1>
         <section className="shop">
           <Option />
           <Option />
@@ -14,9 +14,9 @@ const Payment1 = () => {
           <Option />
           <Option />
           <Option />
+        </section>
       </section>
-    </section>
-      
+
     </>
   )
 }
